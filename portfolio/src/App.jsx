115d1e840f33da056cda67e1b94537bd5767363f
Franco Berlochi <francoberlochi@gmail.com>
2025-11-02 19:41:37 -0300

    #

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css'
 import HomePage from './pages/HomePage';
 
 const router = createBrowserRouter([{ path: "/", element:<HomePage></HomePage> },]);
